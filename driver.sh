c59ebc1a22be49d7bd74beec8f941324a71c85f5
mmeany <mark.meany@os.uk>
2016-10-10 08:35:22 +0100

    Remove TABS

--- driver.sh
+++ driver.sh
@@ -36,26 +36,26 @@ JMETER_OPTS_FILE=${DATADIR}/jmeter.opts
 JMETER_OPTS=""
 
 function validate_env() {
-	if [[ ! -d ${CWD} ]] ; then
-	  echo "The working directory '${CWD}' does not exist"
-		usage
-		exit 1
-	fi
-	if [[ ! -d ${DATADIR} ]] ; then
-	  echo "The data directory '${DATADIR}' does not exist"
-		usage
-		exit 2
-	fi
-	if [[ ! -f ${JMX_SCRIPT} ]] ; then
-	  echo "The script file '${JMX_SCRIPT}' does not exist"
-		usage
-		exit 3
-	fi
-	if [[ ${NUM_SERVERS} -lt 1 ]]; then
-		echo "Must start at least 1 JMX server."
-		usage
-		exit 4
-	fi
+    if [[ ! -d ${CWD} ]] ; then
+      echo "The working directory '${CWD}' does not exist"
+        usage
+        exit 1
+    fi
+    if [[ ! -d ${DATADIR} ]] ; then
+      echo "The data directory '${DATADIR}' does not exist"
+        usage
+        exit 2
+    fi
+    if [[ ! -f ${JMX_SCRIPT} ]] ; then
+      echo "The script file '${JMX_SCRIPT}' does not exist"
+        usage
+        exit 3
+    fi
+    if [[ ${NUM_SERVERS} -lt 1 ]]; then
+        echo "Must start at least 1 JMX server."
+        usage
+        exit 4
+    fi
 }
 
 # Read system and application properties from a file
@@ -68,82 +68,82 @@ function jmeter_opts {
 }
 
 function display_env() {
-	echo "    DATADIR=${DATADIR}"
-	echo " JMX_SCRIPT=${JMX_SCRIPT}"
-	echo "        CWD=${CWD}"
-	echo "NUM_SERVERS=${NUM_SERVERS}"
+    echo "    DATADIR=${DATADIR}"
+    echo " JMX_SCRIPT=${JMX_SCRIPT}"
+    echo "        CWD=${CWD}"
+    echo "NUM_SERVERS=${NUM_SERVERS}"
     echo "JMETER_OPTS=${JMETER_OPTS}"
 }
 
 function start_servers() {
-	n=1
-	while [[ ${n} -le ${NUM_SERVERS} ]]
-	do
-		# Create a log directory for the server
-		LOGDIR=${CWD}/logs/${n}
-	    mkdir -p ${LOGDIR}
+    n=1
+    while [[ ${n} -le ${NUM_SERVERS} ]]
+    do
+        # Create a log directory for the server
+        LOGDIR=${CWD}/logs/${n}
+        mkdir -p ${LOGDIR}
 
-		# Start the server container
-		# docker run --cidfile ${LOGDIR}/cid \
-	    docker run -d -p 0.0.0.0:${HOST_READ_PORT}:1099 -p 0.0.0.0:${HOST_WRITE_PORT}:60000 -v ${LOGDIR}:/logs -v ${DATADIR}:/input_data ${SLAVE_IMAGE} ${JMETER_OPTS} 1>/dev/null 2>&1
-		docker ps
+        # Start the server container
+        # docker run --cidfile ${LOGDIR}/cid \
+        docker run -d -p 0.0.0.0:${HOST_READ_PORT}:1099 -p 0.0.0.0:${HOST_WRITE_PORT}:60000 -v ${LOGDIR}:/logs -v ${DATADIR}:/input_data ${SLAVE_IMAGE} ${JMETER_OPTS} 1>/dev/null 2>&1
+        docker ps
 
-		err=$?
-		if [[ ${err} -ne 0 ]] ; then
-			echo "Error '${err}' while starting a jmeter server. Quitting"
-			exit ${err}
-		fi
+        err=$?
+        if [[ ${err} -ne 0 ]] ; then
+            echo "Error '${err}' while starting a jmeter server. Quitting"
+            exit ${err}
+        fi
 
-		# Prepare for next server
+        # Prepare for next server
         n=$((${n} + 1))
-		HOST_READ_PORT=$((${HOST_READ_PORT} +  2))
-		HOST_WRITE_PORT=$((${HOST_WRITE_PORT} + 2))
-	done
+        HOST_READ_PORT=$((${HOST_READ_PORT} +  2))
+        HOST_WRITE_PORT=$((${HOST_WRITE_PORT} + 2))
+    done
 }
 
 function server_ips() {
-	#
-	# CAUTION: The logic here assumes that we want to use all
-	# active jmeter servers.
-	for pid in $(docker ps | grep ${SLAVE_IMAGE} | awk '{print $1}')
-	do
+    #
+    # CAUTION: The logic here assumes that we want to use all
+    # active jmeter servers.
+    for pid in $(docker ps | grep ${SLAVE_IMAGE} | awk '{print $1}')
+    do
 
-	  # Get the IP for the current pid
-	  x=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${pid})
+      # Get the IP for the current pid
+      x=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${pid})
 
-		# Append to SERVER_IPS
-		if [[ ! -z "${SERVER_IPS}" ]]; then
-			SERVER_IPS=${SERVER_IPS},
-		fi
-		SERVER_IPS=${SERVER_IPS}$x
-	done
+        # Append to SERVER_IPS
+        if [[ ! -z "${SERVER_IPS}" ]]; then
+            SERVER_IPS=${SERVER_IPS},
+        fi
+        SERVER_IPS=${SERVER_IPS}$x
+    done
 }
 
 #
 # Get confirmation
 function confirm() {
-	echo "Here's what we've got..."
-	echo "---------------------------------"
-	display_env
-	echo "---------------------------------"
-	echo "Does this look OK?"
-	select yesorno in "Yes" "No"
-	do
-		case ${yesorno} in
-			Yes ) return ;;
-			No ) exit 6;;
-		esac
-	done
+    echo "Here's what we've got..."
+    echo "---------------------------------"
+    display_env
+    echo "---------------------------------"
+    echo "Does this look OK?"
+    select yesorno in "Yes" "No"
+    do
+        case ${yesorno} in
+            Yes ) return ;;
+            No ) exit 6;;
+        esac
+    done
 }
 
 #
 function usage() {
   echo "Usage:"
-	echo "-d      The data directory for data files used by the JMX script."
-	echo "-h      This help message"
-	echo "-n      The required number of servers"
-	echo "-s      The JMX script file"
-	echo "-w      The working directory. Logs are relative to it."
+    echo "-d      The data directory for data files used by the JMX script."
+    echo "-h      This help message"
+    echo "-n      The required number of servers"
+    echo "-s      The JMX script file"
+    echo "-w      The working directory. Logs are relative to it."
 }
 
 # ------------- Show starts here -------------
@@ -153,17 +153,17 @@ function usage() {
 # script -d data-dir -s script-dir -w work-dir -n num-servers
 while getopts :d:hn:s:w: opt
 do
-	case ${opt} in
-		d) DATADIR=$(readlink -f ${OPTARG}) ;;
-		h) usage && exit 0 ;;
-		n) NUM_SERVERS=${OPTARG} ;;
-		s) JMX_SCRIPT=$(readlink -f ${OPTARG}) ;;
-		w) CWD=$(readlink -f ${OPTARG}) ;;
-		:) echo "The -${OPTARG} option requires a parameter"
-			 exit 1 ;;
-		?) echo "Invalid option: -${OPTARG}"
-			 exit 1 ;;
-	esac
+    case ${opt} in
+        d) DATADIR=$(readlink -f ${OPTARG}) ;;
+        h) usage && exit 0 ;;
+        n) NUM_SERVERS=${OPTARG} ;;
+        s) JMX_SCRIPT=$(readlink -f ${OPTARG}) ;;
+        w) CWD=$(readlink -f ${OPTARG}) ;;
+        :) echo "The -${OPTARG} option requires a parameter"
+             exit 1 ;;
+        ?) echo "Invalid option: -${OPTARG}"
+             exit 1 ;;
+    esac
 done
 shift $((OPTIND -1))
 
@@ -186,12 +186,12 @@ cd ${CWD}
 #
 # Create a place for all the log files
 if [[ -d ${CWD}/logs ]] ; then
-	if [[ -d ${CWD}/logs.bak ]] ; then
-		echo "Unable to backup existing logs dir (backup dir exists)."
-		exit 5
-	else
-		mv ${CWD}/logs ${CWD}/logs.bak
-	fi
+    if [[ -d ${CWD}/logs.bak ]] ; then
+        echo "Unable to backup existing logs dir (backup dir exists)."
+        exit 5
+    else
+        mv ${CWD}/logs ${CWD}/logs.bak
+    fi
 fi
 mkdir -p ${CWD}/logs
 
